73e8f151a30cafb6e6dc654f11017722e7fcb6fd
Adam Volkman (maku) <volkman.adam@gmail.com>
2024-11-11 21:49:24 +0100

    chore(scripts/install): fivetrack

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 apt-get install git npm sqlite3
-git clone https://github.com/Cryptkeeper/Minetrack.git
-cd Minetrack/
+git clone https://github.com/itIsMaku/fivetrack.git
+cd fivetrack/
 npm install --build-from-source
 npm run build
